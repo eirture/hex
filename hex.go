618582fec3ea826487474da2f09c18a915e7e3c8
Jie Liu <eirture@gmail.com>
2023-08-11 23:42:46 +0800

    feat: support position args

--- hex.go
+++ hex.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"io"
 	"os"
+	"strings"
 )
 
 var (
@@ -12,9 +13,16 @@ var (
 )
 
 func main() {
+	flag.Usage = func() {
+		os.Stderr.WriteString("Usage: hex [-d] [string]\n")
+		flag.PrintDefaults()
+	}
 	flag.Parse()
-
 	var r io.Reader = os.Stdin
+	args := flag.Args()
+	if len(args) > 0 {
+		r = strings.NewReader(args[0])
+	}
 	if *decode {
 		io.Copy(os.Stdout, hex.NewDecoder(r))
 	} else {
